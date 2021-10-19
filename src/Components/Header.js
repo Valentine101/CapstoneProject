58a2231b58c7907c6f42f37670cc16719246099d
Grant Barchard <grant.barchard@gmail.com>
2021-10-19 14:39:01 -0400

    changed "alumni site" to "soaring ealges"

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -11,7 +11,7 @@ const Header = () => {
     return (
         <Navbar bg="light" expand="lg">
             <Container>
-                <Navbar.Brand >Alumni Site</Navbar.Brand>
+                <Navbar.Brand >Soaring Eagles</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav"/>
                 <Navbar.Collapse>
                 <Nav>
