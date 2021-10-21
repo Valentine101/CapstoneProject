d3e20609f93279320c7ea6721e806269d1a07e8e
james1amour <38481563+james1amour@users.noreply.github.com>
2021-10-21 16:04:13 -0400

    began modal alumni form

---
+++ src/Components/AlumniForm.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Modal from 'react-bootstrap/Modal'
+
+const AlumniForm = () => {
+    return(
+        <Modal>
+
+        </Modal>
+    )
+}
+
+export default AlumniForm
