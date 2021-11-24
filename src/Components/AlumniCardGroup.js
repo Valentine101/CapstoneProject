78a4802b8e173bc84126b532cf5ba017ccf27ab8
Grant Barchard <grant.barchard@gmail.com>
2021-11-23 19:34:05 -0500

    getAlumniData function, added manage profile modal

--- src/Components/AlumniCardGroup.js
+++ src/Components/AlumniCardGroup.js
@@ -1,25 +1,18 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import AlumniCard from './AlumniCard';
 import Row from 'react-bootstrap/Row'
-//import alumniData from '../data/AlumniData';
+import getAlumniData from '../data/AlumniData';
 import { FilterContext } from '../data/FilterContext';
 import '../Styles/AlumniCardGroup.css';
 
 const AlumniCardGroup = () => {
+    const [alumniData, setAlumniData] = useState([])
     const [filter] = useContext(FilterContext)
 
-    const [alumniData, setAlumniData] = useState([])
-
-
-    //http://localhost:9000/users?page=3&size=2
-    //would return the 5th and 6th result from the sql query
-    fetch('http://localhost:9000/users')
-        .then(res => res.json())
-        // .then(data => obj = data)
-        .then(data => setAlumniData(data))
-        // .then(() => alumniData = obj)*/
-
-    /*
+    useEffect(() =>  {
+        getAlumniData().then(data => setAlumniData(data))
+    }, [])
+    
     function filterCards(alumni) {
         
         function filterString(x) {
@@ -30,7 +23,20 @@ const AlumniCardGroup = () => {
         filteredAlumni = filterString("name")
         filteredAlumni = filterString("major")
         filteredAlumni = filterString("state")
-        filteredAlumni = filterString("sport")
+        // filteredAlumni = filterString("sport")
+
+        filteredAlumni = filteredAlumni.filter(person => {
+            if(person instanceof Array) {
+                person.forEach(sport => {
+                    if(sport.toLowerCase().includes(filter['sport'].toLowerCase())) {
+                        return person
+                    }
+                });
+            }
+            else {
+                person['sport'].toLowerCase().includes(filter['sport'].toLowerCase())
+            }
+        })
 
         if(filter.classAfter !== ""){
             filteredAlumni = filteredAlumni.filter(person => person.class >= parseInt(filter.classAfter))
@@ -41,13 +47,13 @@ const AlumniCardGroup = () => {
 
         return filteredAlumni
     }
-    if(filterCards(alumniData).length === 0){
+    if(alumniData.length === 0){
         return (
             <div style={{fontSize: "50px", textAlign: "center"}}>
                 No Results
             </div>
         )
-    }*/
+    }
     return (
         <Row className="alumni-card-group">
             {(alumniData).map((alumni,index) =>
--- src/Components/ManageProfilePage.js
+++ src/Components/ManageProfilePage.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Table from 'react-bootstrap/Table'
-import alumniData from '../data/AlumniData';
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import getAlumniData from '../data/AlumniData';
 
 
 const ManageProfilePage = () => {
+    const [alumniData, setAlumniData] = useState([])
+    const [selectedProfile, setSelectedProfile] = useState({})
+    
+    const [show, setShow] = useState(false)
+    const open = () => setShow(true)
+    const close = () => setShow(false)
+
+    const [checked, setChecked] = useState(false)
+
+    useEffect(() =>  {
+        getAlumniData().then(data => setAlumniData(data))
+    }, [])
+    
     return (
         <>
             <Table striped boardered hover>
@@ -14,20 +30,39 @@ const ManageProfilePage = () => {
                         <th>Major</th>
                         <th>Sport</th>
                         <th>Location</th>
+                        <th>Admin</th>
                     </tr>
                 </thead>
                 <tbody>
                     {alumniData.map((profile) => 
-                        <tr>
+                        <tr onClick={() => {setSelectedProfile(profile); setChecked(profile.isAdmin); open()}}>
                             <td>{profile.name}</td>
                             <td>{profile.email}</td>
                             <td>{profile.major}</td>
                             <td>{profile.sport}</td>
                             <td>{profile.city}, {profile.state}</td>
+                            <td>{profile.isAdmin ? "Yes" : "No"}</td>
                         </tr>
                     )}
                 </tbody>
             </Table>
+            <Modal show={show} onHide={close}>
+                <Modal.Header closeButton>
+                    <Modal.Title>{selectedProfile.name}</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <Form>
+                        <Form.Check
+                            className="mb-2"
+                            label="Admin"
+                            checked={checked}
+                            // Update db table onChange
+                            onChange={e => setChecked(e.currentTarget.checked)}
+                        />
+                    </Form>
+                    <Button variant="danger">Delete Profile</Button>
+                </Modal.Body>
+            </Modal>
         </>
     )
 }
